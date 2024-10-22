f3fab5c2ba6e32f30164a4367f2ac06e28df64c2
kingsley-sunny <ezeobisunny51@gmail.com>
2024-10-22 11:12:59 +0100

    initial-commint

---
+++ main.js
@@ -0,0 +1,98 @@
+let peerConnection;
+let localStream;
+let remoteStream;
+
+const iceServers = [{ urls: ["stun:stun1.1.google.com:19302", "stun:stun2.1.google.com:19302"] }];
+
+const init = async () => {
+  localStream = await window.navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: {
+      echoCancellation: true,
+      noiseSuppression: true,
+    },
+  });
+
+  document.getElementById("user-1").srcObject = localStream;
+};
+
+const createPeerConnection = async sdpType => {
+  peerConnection = new RTCPeerConnection({
+    iceServers: iceServers,
+  });
+
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = remoteStream;
+
+  // add the local stream to the peer connection
+  localStream.getTracks().forEach(track => {
+    console.log("🚀 ~~ localStream.getTracks ~~ track:", track);
+
+    peerConnection.addTrack(track, localStream);
+  });
+
+  // listen to the peer connection track
+  peerConnection.ontrack = async event => {
+    console.log("🚀 ~~ createOffer ~~ event:", event);
+
+    event.streams[0].getTracks().forEach(track => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  peerConnection.onicecandidate = async event => {
+    if (event.candidate) {
+      console.log("🚀 ~~ createOffer ~~ event:", event);
+      document.getElementById(sdpType).value = JSON.stringify(peerConnection.localDescription);
+    }
+  };
+};
+
+const createOffer = async () => {
+  await createPeerConnection("offer-sdp");
+
+  // create the offer
+  const offer = await peerConnection.createOffer();
+  // set the local description
+  await peerConnection.setLocalDescription(offer);
+
+  // show the offer to the document
+  document.getElementById("offer-sdp").value = JSON.stringify(offer);
+};
+
+const createAnswer = async () => {
+  await createPeerConnection("answer-sdp");
+
+  let offer = document.getElementById("offer-sdp").value;
+
+  if (!offer) return alert("Retrieve offer from peer first");
+  offer = JSON.parse(offer);
+
+  await peerConnection.setRemoteDescription(offer);
+
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+
+  document.getElementById("answer-sdp").value = JSON.stringify(answer);
+};
+
+const addAnswer = async () => {
+  let answer = document.getElementById("answer-sdp").value;
+  if (!answer) return alert("Retrieve answer from peer first");
+
+  answer = JSON.parse(answer);
+  console.log("🚀 ~~ addAnswer ~~ answer:", answer);
+
+  if (!peerConnection.currentRemoteDescription) {
+    peerConnection.setRemoteDescription(answer);
+  }
+};
+
+window.onload = init();
+
+// add event listener
+document.getElementById("create-offer").addEventListener("click", createOffer);
+
+document.getElementById("create-answer").addEventListener("click", createAnswer);
+
+document.getElementById("add-answer").addEventListener("click", addAnswer);
---
+++ peer.js
@@ -0,0 +1,94 @@
+const users = [];
+
+window.onload = async () => {
+  const button = document.querySelector("#connect");
+  const endCall = document.querySelector("#end-call");
+
+  const name = prompt("What is your name");
+
+  // first get the user navigator devices
+
+  const stream = await getUserMedia();
+  console.log("🚀 ~~ window.onload= ~~ stream:", stream);
+
+  // then render the stream
+  renderVideoElement(stream, "main-user");
+
+  const peer = new Peer(name);
+  peer.on("open", id => {
+    users.push(id);
+    renderText(id);
+  });
+
+  peer.on("call", call => {
+    call.answer(stream);
+    console.log("🚀 ~~ window.onload= ~~ on call:", stream?.getVideoTracks()[0]);
+
+    call.on("stream", remoteStream => {
+      console.log("🚀 ~~ window.onload= ~~ remoteStream:", remoteStream?.getVideoTracks()[0]);
+
+      renderVideoElement(remoteStream, "remote-user", false);
+    });
+  });
+
+  peer.on("close", call => {
+    document.getElementById("remote-user").remove();
+  });
+
+  // get the button
+  button.addEventListener("click", async () => {
+    const userId = document.querySelector("#user-id")?.value;
+
+    if (!userId) {
+      alert("Please enter a user-id");
+      return;
+    }
+
+    const call = peer.call(userId, stream);
+
+    call.on("stream", remoteStream => {
+      console.log(
+        "🚀 ~~ button.addEventListener ~~ remoteStream:",
+        remoteStream?.getVideoTracks()[0]
+      );
+      renderVideoElement(remoteStream, "remote-user", false);
+    });
+  });
+
+  endCall.addEventListener("click", async () => {
+    peer.destroy();
+  });
+};
+
+const renderVideoElement = (stream, id, muted = true) => {
+  console.log("🚀 ~~ renderVideoElement ~~ stream:", stream?.getVideoTracks()[0]);
+
+  const videos = document.getElementById("videos");
+  let video = document.getElementById(id);
+  if (!video) {
+    video = document.createElement("video");
+  }
+
+  video.autoplay = true;
+  video.muted = muted;
+  video.srcObject = stream;
+  video.id = id;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+
+  videos.appendChild(video);
+  return video;
+};
+
+const renderText = text => {
+  const p = document.createElement("p");
+  p.innerText = "Name: " + text;
+  document.querySelector("h2").appendChild(p);
+  return p;
+};
+
+const getUserMedia = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  return stream;
+};
